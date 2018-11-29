4d170b7512cbb6f34837e92e7931c5ffcd63f2e4
Georgy Grigoryev <iamkarlson@gmail.com>
2018-11-29 17:38:35 +0100

    Example Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,43 @@
+
+$wakatime = $(where.exe wakatime);
+if($wakatime) {
+    $PLUGIN_VERSION = "0.1";
+}
+
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+
+    $new_pwd = $pwd.ProviderPath.Replace("$home","~");
+
+    Write-Host($new_pwd) -nonewline
+
+    Write-VcsStatus
+
+
+
+    if($wakatime) {
+
+        $gitFolder = (get-item (Get-GitDirectory).Replace(".git",""))
+
+        if($gitFolder){
+
+            try{
+                $command = (Get-History -Count 1|select -Property CommandLine).CommandLine.Split(" ")[0].Replace("(","")
+            } catch{
+                $command = "error"
+            }
+
+            wakatime `
+            --plugin "powershell-wakatime-iamkarlson-plugin/$PLUGIN_VERSION" `
+            --entity-type app `
+            --entity $command `
+            --project $gitFolder.Name `
+            --language PowerShell
+        } else {
+            wakatime --entity-type app --project Powershell;
+        }
+    }
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return "=]"
+}
